8eecd9ded23a7c9153dc52ebaa65325642b21f1e
unknown <magoyeison@gmail.com>
2020-01-06 21:08:54 -0400

    example app 1

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,15 +9,19 @@ import { products } from 'src/app/products';
 })
 export class ProductListComponent implements OnInit {
 
-	products = products;
+    products = products;
 
-  	constructor() { }
+    constructor() { }
 
-  	ngOnInit() {
-  	}	
+    ngOnInit() {
+    }   
 
-  	share() {
-    	window.alert('The product has been shared!');
-  	}
+    share() {
+        window.alert('The product has been shared!');
+    }
+
+    onNotify() {
+        window.alert('You will be notified when the product goes on sale');
+    }
 
 }
